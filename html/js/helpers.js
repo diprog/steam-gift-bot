2be16776458126b7dd2a2573f298b23a625a4551
Dmitry <diprog991@gmail.com>
2023-06-01 00:20:28 +0300

    _

--- html/js/helpers.js
+++ html/js/helpers.js
@@ -1,12 +1,12 @@
 const api_path = '/api';
 
-function api(path, data={}, on_success=null) {
+function api(path, data = {}, on_success = null) {
     return $.ajax(api_path + path, {
         type: 'POST',
         data: data,
         dataType: 'json',
         success: function (result) {
-            if (on_success != null){
+            if (on_success != null) {
                 on_success(result);
             }
 
@@ -17,10 +17,26 @@ function api(path, data={}, on_success=null) {
 }
 
 function setThemeMode() {
-  // Проверяем, является ли тема браузера тёмной
-  if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-      document.documentElement.setAttribute('data-bs-theme', 'dark')
+    // Проверяем, является ли тема браузера тёмной
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
     } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
+        document.documentElement.setAttribute('data-bs-theme', 'light')
     }
+}
+
+function fadeIn(element, done = () => {
+}) {
+    element.hide();
+    element.removeClass('d-none');
+    element.fadeIn(100, done);
+}
+
+function fadeOut(element, done = () => {
+}) {
+    element.fadeOut(100, () => {
+        element.hide();
+        element.addClass('d-none');
+        done();
+    });
 }
